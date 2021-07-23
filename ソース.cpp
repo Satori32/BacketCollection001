4430e8ac8e7511774e78a86e321673ecc533726b
Satori32 <Huga8989+githubf@gmail.com>
2021-07-23 16:56:20 +0900

    add usecase.

--- ソース.cpp
+++ ソース.cpp
@@ -26,23 +26,82 @@ class IFour :public IUnknown {
 public: bool Say() { std::cout << "Four" << std::endl;  return true;}
 };
 
+namespace ID {
+	enum Symbol{
+		None=0,
+		A,
+		B,
+		C,
+	};
+}
+
+enum class Symbol: std::uint8_t {
+	None=0,
+	A,
+	B,
+	C,
+};
+
+
 typedef std::vector<IUnknown::spUnknown> Unknowns;
 typedef std::map<std::uint16_t, Unknowns> M;
+typedef std::map<ID::Symbol, Unknowns> WeekSymbolCollecter;
+typedef std::map<Symbol, Unknowns> StrongSymbolCollecter;
 
 int main() {
-	M M = { {1,{std::make_shared<IOne>()}} };
+	{
+		M M = { {1,{std::make_shared<IOne>()}} };
 
-	M[2].push_back(std::make_shared<ITow>());
+		M[2].push_back(std::make_shared<ITow>());
 
-	M[1].push_back(std::make_shared<IThree>());
+		M[1].push_back(std::make_shared<IThree>());
 
-	M[1].push_back(std::make_shared<IFour>());
+		M[1].push_back(std::make_shared<IFour>());
 
-	for (auto& oo: M) {
-		for (auto& o: oo.second) {
-			o->Say();
+		for (auto& oo : M) {
+			for (auto& o : oo.second) {
+				o->Say();
+			}
+			std::cout << std::endl;
 		}
-		std::cout << std::endl;
+	}
+		
+	std::cout <<"*------------*"<< std::endl;	
+	
+	{
+		WeekSymbolCollecter M = { {ID::A,{std::make_shared<IOne>()}} };
+
+		M[ID::B].push_back(std::make_shared<ITow>());
+
+		M[ID::C].push_back(std::make_shared<IThree>());
+
+		M[ID::A].push_back(std::make_shared<IFour>());
+
+	//	M[16].push_back(std::make_shared<IThree>());
+		for (auto& oo : M) {
+			for (auto& o : oo.second) {
+				o->Say();
+			}
+			std::cout << std::endl;
+		}
+	}
+	std::cout <<"*------------*"<< std::endl;
+	{
+		StrongSymbolCollecter M = { {Symbol::A,{std::make_shared<IOne>()}} };
+		
+		M[Symbol::B].push_back(std::make_shared<ITow>());
+
+		M[Symbol::C].push_back(std::make_shared<IThree>());
+
+		M[Symbol::A].push_back(std::make_shared<IFour>());
+
+		M[Symbol::B].push_back(std::make_shared<IThree>());
+		for (auto& oo : M) {
+			for (auto& o : oo.second) {
+				o->Say();
+			}
+			std::cout << std::endl;
+		}
 	}
 
 }
